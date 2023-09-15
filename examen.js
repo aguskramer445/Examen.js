cbc13ada5b35edf04bbd097eac9c0a462a8ac74e
aguskramer445 <136341916+aguskramer445@users.noreply.github.com>
2023-09-15 08:10:33 -0300

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,9 @@
+let salarioAnual = 21600
+console.log('El salario anual es ' + 21600)
+
+function salarioMensual (salario)
+{
+    let resultado = salarioAnual / 12
+    return resultado
+}
+console.log("El salario mensual es de " + salario (1800))
